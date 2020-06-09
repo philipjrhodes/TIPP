a934263f34c14f20e6730553ee5963e3fb0fcf6c
kevin <kevinmanhn@gmail.com>
2020-06-08 18:30:28 -0700

    Remove directory

--- testIO/freeCluster.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/csh
-
-foreach machine ( worker1 worker2 worker3 worker4 worker5 worker6 worker7 worker8 worker9 worker10 worker11 worker12 worker13 worker14 worker15 worker16 worker17 worker18 worker19 )
-
-        echo $machine
-
-	./free.sh
-end
